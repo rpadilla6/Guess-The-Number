bed65c0190eba713e0d159334207e298eb6e8f60
Ryan <rjpadil1@uci.edu>
2019-08-21 23:27:50 -0700

    Added input component with validation.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 import Card from '../components/Card';
 import Colors from '../constants/colors';
+import Input from '../components/Input';
 
 const StartGameScreen = props => {
+    const [enteredValue, setEnteredValue ] = useState('');
+
+    const numberInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+    };
   return (
     <View style={styles.screen}>
         <Text style={styles.title}>Start a New Game</Text>
         <Card style={styles.inputContainer}>
             <Text>Select a Number</Text>
-            <TextInput/>
+            <Input
+                style={styles.inputBox}
+                blurOnSubmit autocapitalize="none"
+                autoCorrect={false}
+                keyboardType="number-pad"
+                maxLength={2}
+                onChangeText={numberInputHandler}
+                value={enteredValue}
+                />
             <View style={styles.buttonContainer}>
                 <View style={styles.buttonStyles}>
                     <Button  title="Reset" onPress={() => {}} color={Colors.secondary}/>
@@ -46,6 +60,10 @@ const styles = StyleSheet.create({
     },
     buttonStyles: {
         width: 100,
+    },
+    inputBox: {
+        width: 50,
+        textAlign: 'center',
     },
 });
 
