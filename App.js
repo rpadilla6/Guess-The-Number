a1e206687da9a5a2aeb8cd7dc50d08599f365d28
Ryan <rjpadil1@uci.edu>
2019-08-21 22:02:41 -0700

    New game screen added with buttons and text input

--- App.js
+++ App.js
@@ -9,11 +9,13 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
 
 const App = () => {
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
+      <StartGameScreen />
     </View>
   );
 };
