c4c34bfafc2fe2af667c5df32846723244bb5b3f
Ryan <rjpadil1@uci.edu>
2019-08-25 18:08:10 -0700

    Buttons for generating new guesses now work

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import { Text, View, StyleSheet, Button} from 'react-native';
+import React, {useState, useRef, useEffect} from 'react';
+import { Text, View, StyleSheet, Button, Alert} from 'react-native';
 import NumberContainer from './../components/NumberContainer';
 import Card from '../components/Card';
 
@@ -19,13 +19,30 @@ const GameScreen = props => {
     const [currentGuess, setCurrentGuess] = useState(
         generateRandomWithinRange(1,100, props.userChoice)
     );
+    const currentLow = useRef(1);
+    const currentHigh = useRef(100);
+
+    const nextGuessHandler = direction => {
+        if ((direction === 'lower' && currentGuess < props.userChoice) || (direction === 'greater' && currentGuess > props.userChoice)){
+            Alert.alert("Don't lie!", 'You know this is wrong...', [{text: 'Sorry!', style: 'cancel'}]);
+            return;
+        }
+        if (direction === 'lower'){
+            // generateRandomWithinRange(1, props.userChoice)
+            currentHigh.current = currentGuess;
+        } else {
+            currentLow.current = currentGuess;
+        }
+        const nextNumber = generateRandomWithinRange(currentLow.current, currentHigh.current, currentGuess);
+        setCurrentGuess(nextNumber);
+    };
     return (
     <View style={styles.screen}>
         <Text>Opponent's Guess</Text>
         <NumberContainer>{currentGuess}</NumberContainer>
         <Card style={styles.buttonContainer}>
-            <Button title="LOWER" onPress={() => {}} />
-            <Button title="GREATER" onPress={() => {}} />
+            <Button title="LOWER" onPress={nextGuessHandler.bind(this, 'lower')} />
+            <Button title="GREATER" onPress={nextGuessHandler.bind(this, 'greater')} />
         </Card>
     </View>
     );
